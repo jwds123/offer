bc2622bf846f963b885779cdf2c6e6acd5d11b8c
jwds123 <310912887@qq.com>
2018-12-31 12:56:26 +0800

    update

--- 7.重建二叉树.py
+++ 7.重建二叉树.py
@@ -1,3 +1,4 @@
+#o6
 '''
 输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。
 假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
@@ -9,29 +10,60 @@ class TreeNode:
         self.val = x
         self.left = None
         self.right = None
+
 class Solution:
-    # 返回构造的TreeNode根节点
-    def reConstructBinaryTree(self, pre, tin):
+    # 根据前序遍历与中序遍历返回二叉树
+    def reConstructBinaryTree1(self, pre, tin):
         if not pre and not tin:
             return None
         if set(pre) != set(tin):
             return None
         root=TreeNode(pre[0])
         i=tin.index(pre[0])
-        root.left=self.reConstructBinaryTree(pre[1:i+1],tin[:i])
-        root.right=self.reConstructBinaryTree(pre[i+1:],tin[i+1:])
+        root.left=self.reConstructBinaryTree1(pre[1:i+1],tin[:i])
+        root.right=self.reConstructBinaryTree1(pre[i+1:],tin[i+1:])
     
+        return root
+    # 根据后序遍历与中序遍历返回二叉树
+    def reConstructBinaryTree2(self, post, tin):
+        if not post and not tin:
+            return None
+        if set(post) != set(tin):
+            return None
+        root=TreeNode(post[-1])
+        i=tin.index(post[-1])
+        root.left= self.reConstructBinaryTree2(post[0:i], tin[:i])
+        root.right = self.reConstructBinaryTree2(post[i:-1], tin[i + 1:])
+        return root
+
+    # 根据先序遍历与后序遍历返回二叉树
+    def reConstructBinaryTree3(self, pre, post):
+        if not post and not pre:
+            return None
+        if set(post) != set(pre):
+            return None
+        if pre[0]!=post[-1]:
+            return None
+        root = TreeNode(pre[0])
+        if len(pre)>1:
+            i=post.index(pre[1])
+            root.left = self.reConstructBinaryTree3(pre[1:i+2],post[:i+1])
+            root.right = self.reConstructBinaryTree3(pre[i+2:],post[i+1:-1])
         return root
 
 # 按层序遍历输出树中某一层的值
-def PrintNodeAtLevel(treeNode, level):
-    if not treeNode or level < 0:
+def PrintNodeAtLevel(treeNode, depth):
+    '''
+    depth为0就直接输出根节点；
+    递归输出某层的左子树与右子树的值
+    '''
+    if not treeNode or depth < 0:
         return 0
-    if level == 0:
+    if depth == 0:
         print(treeNode.val)
         return 1
-    PrintNodeAtLevel(treeNode.left, level-1)
-    PrintNodeAtLevel(treeNode.right, level-1)
+    PrintNodeAtLevel(treeNode.left, depth-1)
+    PrintNodeAtLevel(treeNode.right, depth-1)
 
 # 已知树的深度按层遍历输出树的值
 def PrintNodeByLevel(treeNode, depth):
@@ -41,10 +73,13 @@ def PrintNodeByLevel(treeNode, depth):
 
 def main():
     sol=Solution()
-    pre = [1,2,4,7,3,5,6,8]
-    tin = [4,7,2,1,5,3,8,6]
-    newTree = sol.reConstructBinaryTree(pre, tin)
-    PrintNodeByLevel(newTree, 3)
+    pre = [5, 3, 2, 1, 4, 7, 6, 8]
+    tin = [1,2,3,4,5,6,7,8]
+    post = [1, 2, 4, 3, 6, 8, 7, 5]
+    newTree = sol.reConstructBinaryTree1(pre, tin)
+    newTree2 = sol.reConstructBinaryTree2(post, tin)
+    newTree3 = sol.reConstructBinaryTree3(pre, post)
+    PrintNodeAtLevel(newTree3, 3)
 
 if __name__ == '__main__':
     main()
